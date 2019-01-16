d5c38451dc026fa53d6361b3ebf0595285c6fc76
Van Waholtz <vanwaholtz@gmail.com>
2019-01-16 22:09:06 +0800

    Some optimization 1. Refactor the setting page and add the enable option. 2. Some bug may have been fixed, but it is not the perfect solution. The new improvement will come sooner or later.
    Signed-off-by: Van Waholtz <vanwaholtz@gmail.com>

--- luasrc/model/cbi/wrtbwmon/config.lua
+++ luasrc/model/cbi/wrtbwmon/config.lua
@@ -1,51 +1,48 @@
-local m = Map("wrtbwmon", translate("Usage - Configuration"))
+local lastvalue
+local cursor = luci.model.uci.cursor()
+local m, s, o
+m = Map("wrtbwmon", translate("Usage - Configuration"))
 
-local s = m:section(NamedSection, "general", "wrtbwmon", translate("General settings"))
+s = m:section(NamedSection, "general", "wrtbwmon", translate("General settings"))
 
-local o = s:option(Value, "path", translate("Database Path"),
-    translate("This box is used to select the Database path, "
-    .. "which is /tmp/usage.db by default."))
+o = s:option(Flag, "enabled", translate("Enabled"))
+o.rmempty= true
+
+o = s:option(Value, "path", translate("Database Path"),
+	translate("This box is used to select the Database path, "
+	.. "which is /tmp/usage.db by default."))
 o:value("/tmp/usage.db")
 o:value("/etc/usage.db")
 o.rmempty= false
 
 function m.on_parse(self)
-    local cursor = luci.model.uci.cursor()
-    local old_path = cursor:get("wrtbwmon", "general", "path")
-    luci.sys.call("uci set wrtbwmon.general.old_path=" .. old_path .. "&& uci commit wrtbwmon" )
+	lastvalue = cursor:get("wrtbwmon", "general", "path")
 end
 
 function o.write(self,section,value)
-    local cursor = luci.model.uci.cursor()
-    local old_path = cursor:get("wrtbwmon", "general", "old_path")
+	local fpath = nixio.fs.dirname(value) .. "/"
 
-    if value:match("^\/") == nil then
-        value = "/tmp/" .. value
-    end
+	if not nixio.fs.access(fpath) then
+		if not nixio.fs.mkdirr(fpath) then
+			return Value.write(self, section, lastvalue)
+		end
+	end
 
-    local cfpath = nixio.fs.dirname(value)
-    local cfname = nixio.fs.basename(value)
-    local new_path = cfpath .. "/" .. cfname
+	io.popen("/etc/init.d/wrtbwmon stop")
+	io.popen("mv -f " .. fileRename(lastvalue, "*") .. " ".. fpath)
+	Value.write(self,section,value)
+	io.popen("/etc/init.d/wrtbwmon start")
 
-    if not nixio.fs.access(cfpath) then
-        if nixio.fs.mkdirr(cfpath) then
-            if nixio.fs.copyr(old_path,new_path) then
-                io.popen("/etc/init.d/wrtbwmon stop")
-                luci.sys.call("mv -f " .. old_path .. " ".. new_path)
-                io.popen("/etc/init.d/wrtbwmon start")
-                return Value.write(self,section,new_path)
-            end
-        end
-    else
-        if nixio.fs.copyr(old_path,new_path) then
-            io.popen("/etc/init.d/wrtbwmon stop")
-            luci.sys.call("mv -f " .. old_path .. " ".. new_path)
-            io.popen("/etc/init.d/wrtbwmon start")
-            return Value.write(self,section,new_path)
-        end
-    end
+	return true
+end
 
-    return Value.write(self,section,old_path)
+function fileRename(fileName, tag)
+	local idx = fileName:match(".+()%.%w+$")
+	if(idx) then
+		return fileName:sub(1, idx-1) .. tag .. fileName:sub(idx, -1)
+	else
+		return fileName .. tag
+	end
 end
 
 return m
