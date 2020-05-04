brvphoenix/luci-app-wrtbwmon

commit a1189f6dca901ab8f0fc38e6cb365ad57137d7e6

Simplify some codes.
Signed-off-by: Van Waholtz <vanwaholtz@gmail.com>

luci-app-wrtbwmon/htdocs/luci-static/resources/view/wrtbwmon/config.js
@@ -20,14 +20,12 @@ return L.view.extend({
 	lastPath: null,
 
 	load: function() {
-		return L.resolveDefault(uci.load('wrtbwmon'), null).then(function(res) {
-			return L.resolveDefault(uci.get('wrtbwmon', 'general', 'path'), null);
-		});
+		return uci.load('wrtbwmon');
 	},
 
-	render: function(data) {
+	render: function() {
+		this.lastPath = uci.get_first('wrtbwmon', 'general', 'path') || null;
 		var m, s, o;
-		this.lastPath = data;
 
 		m = new form.Map('wrtbwmon', _('Usage - Configuration'));
 

luci-app-wrtbwmon/htdocs/luci-static/resources/view/wrtbwmon/details.js
@@ -179,15 +179,13 @@ function registerTableEventHandlers() {
 
 function resolveCustomizedHostName() {
 	var hostNameFile = '/etc/wrtbwmon.user'
-	return Promise.resolve(fs.stat(hostNameFile)).then(function() {
-		return L.resolveDefault(fs.trimmed(hostNameFile), []).then(function(hostNames) {
-			return hostNames.split(/\r?\n|\r/g).map(function(res) {
-				var data = res.split(',');
-				return {
-					macaddr: data[0],
-					hostname: data[1]
-				};
-			})
+	return L.resolveDefault(fs.trimmed(hostNameFile), []).then(function(hostNames) {
+		return hostNames.split(/\r?\n|\r/g).map(function(res) {
+			var data = res.split(',');
+			return {
+				macaddr: data[0],
+				hostname: data[1]
+			};
 		})
 	})
 }

commit 011ac73ff51715d5fd806c7b3a5548b69641d672

Fix bugs introduced by a1189f6. These bugs lead to modify the savepath unsuccessfully.
Signed-off-by: Van Waholtz <vanwaholtz@gmail.com>

luci-app-wrtbwmon/htdocs/luci-static/resources/view/wrtbwmon/config.js
@@ -20,11 +20,12 @@ return L.view.extend({
 	lastPath: null,
 
 	load: function() {
-		return uci.load('wrtbwmon');
+		return uci.load('wrtbwmon').then(L.bind(function() {
+			this.lastPath = uci.get_first('wrtbwmon', 'wrtbwmon', 'path') || null;
+		}, this));
 	},
 
 	render: function() {
-		this.lastPath = uci.get_first('wrtbwmon', 'general', 'path') || null;
 		var m, s, o;
 
 		m = new form.Map('wrtbwmon', _('Usage - Configuration'));

luci-app-wrtbwmon/htdocs/luci-static/resources/view/wrtbwmon/details.js
@@ -17,9 +17,12 @@ function $(tid) {
 }
 
 function getPath() {
-	return Promise.resolve(uci.load('wrtbwmon')).then(function() {
-		return L.resolveDefault(uci.get('wrtbwmon', 'general', 'path'), null);
-	})
+	return uci.load('wrtbwmon').then(function() {
+		return L.resolveDefault(uci.get_first('wrtbwmon', 'wrtbwmon', 'path'), null);
+	}).then(function(res) {
+		uci.unload('wrtbwmon');
+		return res;
+	});
 }
 
 function formatDate(date) {